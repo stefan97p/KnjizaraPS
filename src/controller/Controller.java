06892733c054a31347eddbe78e9f2c831818f34d
Stefan Jokanovic <stefan97pro@gmail.com>
2024-04-01 15:01:04 +0200

    Dodavanje osnovnih CRUD operacija

---
+++ src/controller/Controller.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Autor;
+import model.Knjiga;
+import model.Zanr;
+
+/**
+ *
+ * @author Ne
+ */
+public class Controller {
+    private List<Knjiga>listaKnjiga = new ArrayList<>();
+    private List<Autor>listaAutora = new ArrayList<>();
+
+    private static Controller instance;
+    public static Controller getInstance(){
+        if(instance==null){
+            instance = new Controller();
+        }
+        return instance;
+    }
+    
+    private Controller() {
+          
+        Autor a1 = new Autor("Ivo", "Andric", 1892, "Biografija");
+        Autor a2 = new Autor("Danilo", "Kis", 1935, "Biografijaa je tto");
+        Autor a3 = new Autor("Milos", "Peric", 1985, "Biografija i ostalo");
+        
+        Knjiga k1 = new Knjiga("Na Drini Cuprija", a1, "12345315", 1945, Zanr.ISTORIJSKI);
+        Knjiga k2 = new Knjiga("Basta", a2, "52125315", 1982, Zanr.ROMAN);
+        Knjiga k3 = new Knjiga("Detektiv Pera", a3, "007", 2003, Zanr.DETEKTIVSKI);
+        
+
+        
+        listaAutora.add(a1);
+        listaAutora.add(a2);
+        listaAutora.add(a3);
+        
+        listaKnjiga.add(k1);
+        listaKnjiga.add(k2);
+        listaKnjiga.add(k3);
+        
+    }
+
+    public List<Knjiga> getListaKnjiga() {
+        return listaKnjiga;
+    }
+
+    public void setListaKnjiga(List<Knjiga> listaKnjiga) {
+        this.listaKnjiga = listaKnjiga;
+    }
+
+    public List<Autor> getListaAutora() {
+        return listaAutora;
+    }
+
+    public void setListaAutora(List<Autor> listaAutora) {
+        this.listaAutora = listaAutora;
+    }
+
+    public void obrisiKnjigu(int selektovaniRed) {
+        listaKnjiga.remove(selektovaniRed);
+    }
+
+    public void dodajKnjigu(Knjiga novaKnjiga) {
+        listaKnjiga.add(novaKnjiga);
+    }
+    
+    
+    
+}
